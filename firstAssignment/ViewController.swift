fe9a784cca79421da6eea7cda5d49929b51f4815
enoch232 <enoch232@gmail.com>
2017-01-17 22:46:12 -0700

    first commit

--- firstAssignment/ViewController.swift
+++ firstAssignment/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let height = Float(self.heightField.text!)!
+        let weight = Float(self.weightField.text!)!
+        let bmi = (weight/((height)*(height)))*703
+        self.bmiLabel.text = String(format: "%.5f", bmi)
+        if bmi < 18 {
+            self.resultLabel.text = "You are Underweight"
+            self.resultLabel.textColor = UIColor.blue
+        }else if bmi < 25 && bmi >= 18{
+            self.resultLabel.text = "You are Normal"
+            self.resultLabel.textColor = UIColor.green
+        }else if bmi <= 30 && bmi >= 25{
+            self.resultLabel.text = "You are Pre-Obese"
+            self.resultLabel.textColor = UIColor.purple
+        }else if bmi > 30{
+            self.resultLabel.text = "You are Obese"
+            self.resultLabel.textColor = UIColor.red
+        }
+        
+        
+    }
 
 }
 
